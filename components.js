2c5c1ac806d9630f6def4b92b2bb55bb987bb894
Geovane Filho <geo.vane.20002@gmail.com>
2023-12-05 14:40:29 -0300

    finished phase 1

--- components.js
+++ components.js
@@ -10,3 +10,35 @@ AFRAME.registerComponent("phase-shift", {
     });
   },
 });
+
+AFRAME.registerComponent("collision-detector", {
+  tick: function () {
+    const thisEl = this.el;
+    const otherEntities = document.querySelectorAll(
+      "[collision-detector]:not(.ignore-collision)"
+    );
+
+    otherEntities.forEach(function (entity) {
+      if (thisEl !== entity && areEntitiesColliding(thisEl, entity)) {
+        console.log("Os objetos estão sobrepostos!");
+        if (entity.hasAttribute("dynamic-body")) {
+          entity.removeAttribute("dynamic-body");
+          entity.setAttribute("static-body", "");
+        }
+      }
+    });
+  },
+});
+
+function areEntitiesColliding(entity1, entity2) {
+  const pos1 = entity1.object3D.position;
+  const pos2 = entity2.object3D.position;
+  const size1 = entity1.getAttribute("scale");
+  const size2 = entity2.getAttribute("scale");
+
+  return (
+    Math.abs(pos1.x - pos2.x) * 2 < size1.x + size2.x &&
+    Math.abs(pos1.y - pos2.y) * 2 < size1.y + size2.y &&
+    Math.abs(pos1.z - pos2.z) * 2 < size1.z + size2.z
+  );
+}
